b8e3c16be6c2b136d27f7269278dc66a41b32ad8
Tuan Nguyen <tuan@scalapay.com>
2022-08-14 21:37:29 +0700

    fix(is-odd): fix is-odd package

--- packages/is-odd/src/cli/is-odd.ts
+++ packages/is-odd/src/cli/is-odd.ts
@@ -10,4 +10,4 @@ program.parse(process.argv);
 const options = program.opts();
 
 // eslint-disable-next-line no-console
-console.log('Is odd?', isOdd(Number(options.number)));
+console.log('Is odd', isOdd(Number(options.number)));
