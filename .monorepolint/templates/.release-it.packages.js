34e101e6c3fee78679a727c1dfafcb69774769ac
Tuan Nguyen <tuan@scalapay.com>
2022-08-14 22:08:19 +0700

    fix(release): extract common release plan

--- .monorepolint/templates/.release-it.packages.js
+++ .monorepolint/templates/.release-it.packages.js
@@ -1,53 +1,7 @@
 /* eslint-disable */
-const packageJson = require('./package.json');
 
-const name = packageJson.name.split('/')[1];
-
-module.exports = {
-	"plugins": {
-	  "@release-it/conventional-changelog": {
-		"preset": {
-		  "name": "conventionalcommits",
-		  "types": [
-			{
-			  "type": "feat",
-			  "section": "Features"
-			},
-			{
-			  "type": "fix",
-			  "section": "Bug Fixes"
-			},
-			{
-			  "type": "cleanup",
-			  "section": "Cleanup"
-			},
-			{
-			  "type": "docs",
-			  "section": "Documentations"
-			}
-		  ]
-		},
-		"gitRawCommitsOpts": {
-			"path": __dirname,
-		},
-		"lernaPackage": name,
-		"path": __dirname,
-		"infile": "./CHANGELOG.md"
-	  },
-	  "@release-it/bumper": {
-		"in": "./package.json",
-		"out": ["./version.json"]
-	  }
+module.exports = require('../../.release-it.js')(__dirname, {
+	npm: {
+		publish: true,
 	},
-	"git": {
-	  "commitMessage": `chore(repo): release ${name} \${version}`,
-	  "tagName": `${name}@\${version}`
-	},
-	"npm": {
-	  "publish": true
-	},
-	"github": {
-	  "release": true,
-	  "releaseName": `Release: ${name} \${version}`
-	}
-  }
+});
---
+++ .release-it.js
@@ -0,0 +1,89 @@
+/* eslint-disable */
+const merge = require('lodash.merge');
+
+module.exports = (dirname, overrides = {}) => {
+	const packageJson = require(`${dirname}/package.json`);
+	const name = packageJson.name.split('/')[1];
+
+	return merge(
+		{
+			plugins: {
+				'@release-it/conventional-changelog': {
+					preset: {
+						name: 'conventionalcommits',
+						types: [
+							{
+								type: 'feat',
+								section: 'Features',
+							},
+							{
+								type: 'fix',
+								section: 'Bug Fixes',
+							},
+							{
+								type: 'cleanup',
+								section: 'Cleanup',
+							},
+							{
+								type: 'docs',
+								section: 'Documentations',
+							},
+						],
+					},
+					gitRawCommitsOpts: {
+						path: dirname,
+					},
+					lernaPackage: name,
+					path: dirname,
+					whatBump: (commits) => {
+						if (!commits.length) {
+							return {
+								level: null,
+							};
+						}
+						let level = 2;
+						let breakings = 0;
+						let features = 0;
+
+						commits.forEach((commit) => {
+							if (commit.notes.length > 0) {
+								breakings += commit.notes.length;
+								level = 0;
+							} else if (commit.type === 'feat') {
+								features += 1;
+								if (level === 2) {
+									level = 1;
+								}
+							}
+						});
+
+						return {
+							level: level,
+							reason:
+								breakings === 1
+									? `There is ${breakings} BREAKING CHANGE and ${features} features`
+									: `There are ${breakings} BREAKING CHANGES and ${features} features`,
+						};
+					},
+					infile: './CHANGELOG.md',
+				},
+				'@release-it/bumper': {
+					in: './package.json',
+					out: ['./version.json'],
+				},
+			},
+			git: {
+				commitMessage: `chore(repo): release ${name} \${version}`,
+				tagName: `${name}@\${version}`,
+			},
+			npm: {
+				publish: true,
+			},
+			github: {
+				release: true,
+				releaseName: `Release: ${name} \${version}`,
+			},
+		},
+		overrides
+	);
+};
